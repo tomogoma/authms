68b657e9c84bfb8ffeabf8e5b6e03c33e66f7ad9
tomogoma <ogomatom@gmail.com>
2016-06-22 22:44:22 +0300

    Set up for auth tests

---
+++ auth/auth_test.go
@@ -0,0 +1,21 @@
+package auth_test
+
+import "testing"
+
+func TestNew(t *testing.T) {
+
+	// TODO save login details for all auth types
+	// 1. Successful register
+	// 2. Invalid (unsuccessful register)
+	// 3. Successful login
+	// 4. Invalid unsuccessful login
+	// 5. Successful token validation
+	// 6. Invalid (unsuccessful) token validation
+
+	//  TODO limit auth attempts when unsuccessful
+	// 1. n login/token attempts from an ip address for a user over duration t
+	// 2. m>n login/token attempts for a user over duration t
+	// 3. p>m login/token attempts from an ip address over duration t
+
+	t.Fatalf("Not yet implemented")
+}
