13af868b8524635bd1f3f5868141f2458244720b
tomogoma <ogomatom@gmail.com>
2016-11-14 22:06:09 +0300

    Fix reference to uninstall script

--- install/systemd-install.sh
+++ install/systemd-install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-./uninstall.sh || exit 1
-
 NAME="authms"
 BUILD_NAME="${NAME}-installer-version"
 CONF_DIR="/etc/${NAME}"
@@ -9,16 +7,13 @@ CONF_FILE="${CONF_DIR}/${NAME}.conf.yml"
 INSTALL_DIR="/usr/local/bin"
 UNIT_FILE="/etc/systemd/system/${NAME}.service"
 
+./systemd-uninstall.sh || exit 1
 echo "Begin install"
-
 mkdir -p "${CONF_DIR}" || exit 1
 cp "${NAME}.conf.yml" "${CONF_FILE}" || exit 1
-
 mkdir -p "${INSTALL_DIR}" || exit 1
 cp -f "${BUILD_NAME}" "${INSTALL_DIR}/${NAME}" || exit 1
-
 cp -f "${NAME}.service" "${UNIT_FILE}" || exit 1
 systemctl enable "${NAME}.service"
-
 echo "Config file is at '${CONF_FILE}"
 echo "Install complete"
