1f98ab1937b2e4b71867fd89f4f2483347dab27b
tomogoma <ogomatom@gmail.com>
2016-11-16 17:31:11 +0300

    fixed printing secret bug, validate facebook id too

--- auth/oauth/config.go
+++ auth/oauth/config.go
@@ -8,10 +8,14 @@ type Config struct {
 }
 
 var ErrorEmptyFacebookSecret = errors.New("facebook secret was empty")
+var ErrorBadFacebookID = errors.New("facebook id was invalid")
 
 func (c Config) Validate() error {
 	if c.FacebookSecret == "" {
 		return ErrorEmptyFacebookSecret
+	}
+	if c.FacebookID < 1 {
+		return ErrorBadFacebookID
 	}
 	return nil
 }
