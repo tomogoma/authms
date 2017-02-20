d977f916bdc6bba4839eb2a254cdd92bfef21862
tomogoma <ogomatom@gmail.com>
2017-02-20 20:53:55 +0300

    add sms functionality

--- auth/phone/sms/sms.go
+++ auth/phone/sms/sms.go
@@ -1,1 +1,86 @@
 package sms
+
+import (
+	"net/http"
+	"github.com/tomogoma/go-commons/errors"
+	"path"
+	"io/ioutil"
+	"net/url"
+	"strings"
+)
+
+const (
+	accountsURL = "https://api.twilio.com/2010-04-01/Accounts/"
+	messagesPath = "Messages.json"
+	httpMethod = "POST"
+	formKeyTo = "To"
+	formKeyFrom = "From"
+	formKeyBody = "Body"
+	testMessage = "this is a test message"
+)
+
+type Config interface {
+	TwilioID() string
+	TwilioTokenKeyFile() string
+	TwilioSenderPhone() string
+	TwilioTestNumber() string
+}
+
+type SMS struct {
+	token  string
+	config Config
+}
+
+func New(c Config) (*SMS, error) {
+	if c == nil {
+		return nil, errors.New("Config was nil")
+	}
+	token, err := readToken(c.TwilioTokenKeyFile())
+	if err != nil {
+		return nil, err
+	}
+	sms := &SMS{config:c, token:token}
+	if err := sms.SMS(c.TwilioTestNumber(), testMessage); err != nil {
+		return nil, errors.Newf("Unable to access twilio, probably" +
+			" SMS config values are invalid?: %s", err)
+	}
+	return sms, nil
+}
+
+func (s *SMS) SMS(toPhone, message string) error {
+	client := &http.Client{}
+	URL, err := url.Parse(accountsURL)
+	if err != nil {
+		return errors.Newf("problem with the twilio URL (%v)" +
+			" contact support", err)
+	}
+	URL.Path = path.Join(URL.Path, s.config.TwilioID(), messagesPath)
+	form := url.Values{}
+	form.Add(formKeyTo, toPhone)
+	form.Add(formKeyFrom, s.config.TwilioSenderPhone())
+	form.Add(formKeyBody, message)
+	req, err := http.NewRequest(httpMethod, URL.String(), strings.NewReader(form.Encode()))
+	if err != nil {
+		return errors.Newf("unable to create message request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(s.config.TwilioID(), s.token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.Newf("unable to perform request: %v", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if resp.StatusCode >= 400 {
+		return errors.Newf("%s: %s", resp.Status, body)
+	}
+	return nil
+}
+
+func readToken(tokenKeyFile string) (string, error) {
+	fb, err := ioutil.ReadFile(tokenKeyFile)
+	if err != nil {
+		return "", errors.Newf("error opening twilio token file: %v", err)
+	}
+	return string(fb), nil
+}
---
+++ auth/phone/sms/sms_test.go
@@ -0,0 +1,51 @@
+package sms_test
+
+import (
+	"testing"
+	"github.com/tomogoma/authms/auth/phone/sms"
+	"flag"
+	"github.com/tomogoma/go-commons/config"
+)
+
+type ConfigMock struct {
+	ID           string `json:"ID" yaml:"ID"`
+	SenderPhone  string `json:"senderPhone" yaml:"senderPhone"`
+	TokenKeyFile string `json:"tokenKeyFile" yaml:"tokenKeyFile"`
+	TestNumber   string `json:"testNumber" yaml:"testNumber"`
+}
+
+func (c ConfigMock) TwilioID() string {
+	return c.ID
+}
+func (c ConfigMock) TwilioTokenKeyFile() string {
+	return c.TokenKeyFile
+}
+func (c ConfigMock) TwilioSenderPhone() string {
+	return c.SenderPhone
+}
+
+func (c ConfigMock) TwilioTestNumber() string {
+	return c.TestNumber
+}
+
+type AuthMsConfigMock struct {
+	Twilio ConfigMock `json:"twilio" yaml:"twilio"`
+}
+
+var confFile = flag.String("conf", "/etc/authms/authms.conf.yml", "/path/to/config.conf.yml")
+
+func init() {
+	flag.Parse()
+}
+
+func Test(t *testing.T) {
+	conf := AuthMsConfigMock{}
+	err := config.ReadYamlConfig(*confFile, &conf)
+	if err != nil {
+		t.Fatalf("Error setting up (reading config file): %v", err)
+	}
+	_, err = sms.New(conf.Twilio)
+	if err != nil {
+		t.Fatalf("sms.New(): %v", err)
+	}
+}
