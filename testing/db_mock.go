ae372b5af13197d86f06efb9d93f38c56a287bca
tomogoma <ogomatom@gmail.com>
2017-10-07 23:57:52 +0300

    Detect direct calls to db while in tx

--- testing/db_mock.go
+++ testing/db_mock.go
@@ -83,46 +83,72 @@ type DBMock struct {
 	ExpMailTknsErr      error
 
 	ExpInsFbAtmErr error
+
+	isInTx bool
 }
 
 func (db *DBMock) ExecuteTx(fn func(*sql.Tx) error) error {
+	db.isInTx = true
+	defer func() {
+		db.isInTx = false
+	}()
 	return fn(new(sql.Tx))
 }
 
 func (db *DBMock) GroupByName(string) (*model.Group, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpGrpBNm == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpGrpBNm, db.ExpGrpBNmErr
 }
+
 func (db *DBMock) Group(string) (*model.Group, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpGrp == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpGrp, db.ExpGrpErr
 }
+
 func (db *DBMock) InsertGroup(name string, acl float32) (*model.Group, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsGrpErr != nil {
 		return nil, db.ExpInsGrpErr
 	}
 	return &model.Group{ID: currentID(), Name: name, AccessLevel: acl}, db.ExpInsGrpErr
 }
+
 func (db *DBMock) AddUserToGroupAtomic(tx *sql.Tx, userID, groupID string) error {
 	return db.ExpAddUsrTGrpAtmcErr
 }
 
 func (db *DBMock) UserTypeByName(string) (*model.UserType, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsrTypBNm == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUsrTypBNm, db.ExpUsrTypBNmErr
 }
+
 func (db *DBMock) InsertUserType(name string) (*model.UserType, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsUsrTypErr != nil {
 		return nil, db.ExpInsUsrTypErr
 	}
 	return &model.UserType{ID: currentID(), Name: name}, db.ExpInsUsrTypErr
 }
+
 func (db *DBMock) InsertUserAtomic(tx *sql.Tx, t model.UserType, password []byte) (*model.User, error) {
 	if db.ExpInsUsrAtmErr != nil {
 		return nil, db.ExpInsUsrAtmErr
@@ -131,12 +157,19 @@ func (db *DBMock) InsertUserAtomic(tx *sql.Tx, t model.UserType, password []byte
 }
 
 func (db *DBMock) APIKeysByUserID(userID string, offset, count int64) ([]api.Key, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpAPIKsBUsrID == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpAPIKsBUsrID, db.ExpAPIKsBUsrIDErr
 }
+
 func (db *DBMock) InsertAPIKey(userID, key string) (*api.Key, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsAPIKErr != nil {
 		return nil, db.ExpInsAPIKErr
 	}
@@ -144,18 +177,29 @@ func (db *DBMock) InsertAPIKey(userID, key string) (*api.Key, error) {
 }
 
 func (db *DBMock) UpdateUsername(userID, username string) (*model.Username, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUpdUsrNm == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUpdUsrNm, db.ExpUpdUsrNmErr
 }
+
 func (db *DBMock) UpdateUserPhone(userID, phone string, verified bool) (*model.VerifLogin, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUpdUsrPhn == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUpdUsrPhn, db.ExpUpdUsrPhnErr
 }
+
 func (db *DBMock) UpdateUserEmail(userID, email string, verified bool) (*model.VerifLogin, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUpdUsrMail == nil {
 		return nil, errors.NewNotFound("not found")
 	}
@@ -163,17 +207,23 @@ func (db *DBMock) UpdateUserEmail(userID, email string, verified bool) (*model.V
 }
 
 func (db *DBMock) UpdatePassword(userID string, password []byte) error {
+	if db.isInTx {
+		return errors.Newf("direct db call while in tx")
+	}
 	return db.ExpupdPassErr
 }
+
 func (db *DBMock) UpdatePasswordAtomic(tx *sql.Tx, userID string, password []byte) error {
 	return db.ExpupdPassAtmErr
 }
+
 func (db *DBMock) UpdateUserPhoneAtomic(tx *sql.Tx, userID, phone string, verified bool) (*model.VerifLogin, error) {
 	if db.ExpUpdUsrPhnAtm == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUpdUsrPhnAtm, db.ExpUpdUsrPhnAtmErr
 }
+
 func (db *DBMock) UpdateUserEmailAtomic(tx *sql.Tx, userID, email string, verified bool) (*model.VerifLogin, error) {
 	if db.ExpUpdUsrMailAtm == nil {
 		return nil, errors.NewNotFound("not found")
@@ -182,47 +232,63 @@ func (db *DBMock) UpdateUserEmailAtomic(tx *sql.Tx, userID, email string, verifi
 }
 
 func (db *DBMock) InsertUserPhone(userID, phone string, verified bool) (*model.VerifLogin, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsUsrPhnErr != nil {
 		return nil, db.ExpInsUsrPhnErr
 	}
 	return &model.VerifLogin{ID: currentID(), UserID: userID, Address: phone, Verified: verified}, db.ExpInsUsrPhnErr
 }
+
 func (db *DBMock) InsertUserEmail(userID, email string, verified bool) (*model.VerifLogin, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsUsrMailErr != nil {
 		return nil, db.ExpInsUsrMailErr
 	}
 	return &model.VerifLogin{ID: currentID(), UserID: userID, Address: email, Verified: verified}, db.ExpInsUsrMailErr
 }
+
 func (db *DBMock) InsertUserName(userID, username string) (*model.Username, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsUsrNmErr != nil {
 		return nil, db.ExpInsUsrNmErr
 	}
 	return &model.Username{ID: currentID(), UserID: userID, Value: username}, db.ExpInsUsrNmErr
 }
+
 func (db *DBMock) InsertUserPhoneAtomic(tx *sql.Tx, userID, phone string, verified bool) (*model.VerifLogin, error) {
 	if db.ExpInsUsrPhnAtmErr != nil {
 		return nil, db.ExpInsUsrPhnAtmErr
 	}
 	return &model.VerifLogin{ID: currentID(), UserID: userID, Address: phone, Verified: verified}, db.ExpInsUsrPhnAtmErr
 }
+
 func (db *DBMock) InsertUserEmailAtomic(tx *sql.Tx, userID, email string, verified bool) (*model.VerifLogin, error) {
 	if db.ExpInsUsrMailAtmErr != nil {
 		return nil, db.ExpInsUsrMailAtmErr
 	}
 	return &model.VerifLogin{ID: currentID(), UserID: userID, Address: email, Verified: verified}, db.ExpInsUsrMailAtmErr
 }
+
 func (db *DBMock) InsertUserNameAtomic(tx *sql.Tx, userID, username string) (*model.Username, error) {
 	if db.ExpInsUsrNmAtmErr != nil {
 		return nil, db.ExpInsUsrNmAtmErr
 	}
 	return &model.Username{ID: currentID(), UserID: userID, Value: username}, db.ExpInsUsrNmAtmErr
 }
+
 func (db *DBMock) InsertUserFbIDAtomic(tx *sql.Tx, userID, fbID string, verified bool) (*model.Facebook, error) {
 	if db.ExpInsFbAtmErr != nil {
 		return nil, db.ExpInsFbAtmErr
 	}
 	return &model.Facebook{ID: currentID(), UserID: userID, FacebookID: fbID, Verified: verified}, db.ExpInsFbAtmErr
 }
+
 func (db *DBMock) InsertUserDeviceAtomic(tx *sql.Tx, userID, devID string) (*model.Device, error) {
 	if db.ExpInsDevAtmErr != nil {
 		return nil, db.ExpInsDevAtmErr
@@ -231,36 +297,53 @@ func (db *DBMock) InsertUserDeviceAtomic(tx *sql.Tx, userID, devID string) (*mod
 }
 
 func (db *DBMock) InsertPhoneToken(userID, phone string, dbt []byte, isUsed bool, expiry time.Time) (*model.DBToken, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsPhnTknErr != nil {
 		return nil, db.ExpInsPhnTknErr
 	}
 	return &model.DBToken{ID: currentID(), UserID: userID, Address: phone, Token: dbt}, db.ExpInsPhnTknErr
 }
+
 func (db *DBMock) InsertEmailToken(userID, email string, dbt []byte, isUsed bool, expiry time.Time) (*model.DBToken, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpInsMailTknErr != nil {
 		return nil, db.ExpInsMailTknErr
 	}
 	return &model.DBToken{ID: currentID(), UserID: userID, Address: email, Token: dbt}, db.ExpInsMailTknErr
 }
+
 func (db *DBMock) InsertPhoneTokenAtomic(tx *sql.Tx, userID, phone string, dbt []byte, isUsed bool, expiry time.Time) (*model.DBToken, error) {
 	if db.ExpInsPhnTknAtmErr != nil {
 		return nil, db.ExpInsPhnTknAtmErr
 	}
 	return &model.DBToken{ID: currentID(), UserID: userID, Address: phone, Token: dbt}, db.ExpInsPhnTknAtmErr
 }
+
 func (db *DBMock) InsertEmailTokenAtomic(tx *sql.Tx, userID, email string, dbt []byte, isUsed bool, expiry time.Time) (*model.DBToken, error) {
 	if db.ExpInsMailTknAtmErr != nil {
 		return nil, db.ExpInsMailTknAtmErr
 	}
 	return &model.DBToken{ID: currentID(), UserID: userID, Address: email, Token: dbt}, db.ExpInsMailTknAtmErr
 }
+
 func (db *DBMock) PhoneTokens(userID string, offset, count int64) ([]model.DBToken, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if len(db.ExpPhnTkns) == 0 {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpPhnTkns, db.ExpPhnTknsErr
 }
+
 func (db *DBMock) EmailTokens(userID string, offset, count int64) ([]model.DBToken, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if len(db.ExpMailTkns) == 0 {
 		return nil, errors.NewNotFound("not found")
 	}
@@ -268,36 +351,59 @@ func (db *DBMock) EmailTokens(userID string, offset, count int64) ([]model.DBTok
 }
 
 func (db *DBMock) User(id string) (*model.User, []byte, error) {
+	if db.isInTx {
+		return nil, nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsr == nil {
 		return nil, nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUsr, db.ExpUsrPass, db.ExpUsrErr
 }
+
 func (db *DBMock) UserByPhone(phone string) (*model.User, []byte, error) {
+	if db.isInTx {
+		return nil, nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsrBPhn == nil {
 		return nil, nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUsrBPhn, db.ExpUsrBPhnPass, db.ExpUsrBPhnErr
 }
+
 func (db *DBMock) UserByEmail(email string) (*model.User, []byte, error) {
+	if db.isInTx {
+		return nil, nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsrBMail == nil {
 		return nil, nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUsrBMail, db.ExpUsrBMailPass, db.ExpUsrBMailErr
 }
+
 func (db *DBMock) UserByUsername(username string) (*model.User, []byte, error) {
+	if db.isInTx {
+		return nil, nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsrBUsrNm == nil {
 		return nil, nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUsrBUsrNm, db.ExpUsrBUsrNmPass, db.ExpUsrBUsrNmErr
 }
+
 func (db *DBMock) UserByFacebook(facebookID string) (*model.User, error) {
+	if db.isInTx {
+		return nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsrBFb == nil {
 		return nil, errors.NewNotFound("not found")
 	}
 	return db.ExpUsrBFb, db.ExpUsrBFbErr
 }
+
 func (db *DBMock) UserByDeviceID(devID string) (*model.User, []byte, error) {
+	if db.isInTx {
+		return nil, nil, errors.Newf("direct db call while in tx")
+	}
 	if db.ExpUsrBDev == nil {
 		return nil, nil, errors.NewNotFound("not found")
 	}
